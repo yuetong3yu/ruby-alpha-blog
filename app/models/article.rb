b802162cb049727074f10bcf37ff8abd7c0bba64
yuetong3yu <yuetong3yu@gmail.com>
2022-11-08 10:00:00 +0800

    article validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 6, maximum: 30 }
+  validates :description, presence: true, length: { minimum: 1, maximum: 300 }
 end
