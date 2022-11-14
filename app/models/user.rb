bab24854900118d35439db6fad64daf1027ac6ec
yuetong3yu <yuetong3yu@gmail.com>
2022-11-14 15:59:38 +0800

    user add secure password diagest

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
                       format: { with: EMAIL_REGEX }
+  has_secure_password
 end
