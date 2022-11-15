cfc0da91be227b949eb0b0a2dfa5461e7ff701bf
yuetong3yu <yuetong3yu@gmail.com>
2022-11-15 11:17:42 +0800

    TDD a name presence

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,23 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
+  def setup 
+    @category = Category.new(name: 'Sports')
+  end
+
   test "Category should be valid" do
-    @category = Category.new(name: 'hi')
     assert @category.valid?
   end
+
+  test "Category name should be valid" do
+    assert @category.name === 'Sports' 
+  end
+
+  test "Category should have valid name" do
+    @category = Category.new(name: "    ")
+    assert_not @category.valid?
+  end
+
+
 
 end
